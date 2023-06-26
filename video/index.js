4c9be6a871c7a272febebc8b1fa3ce4c4c816f47
hossainchisty <hossain.chisty11@gmail.com>
2023-06-27 00:17:22 +0600

    refactor: Added dynamic port

--- video/index.js
+++ video/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = 3003
 
 app.get('/', (req, res) => {
   res.send(`Hello from video service`);
 });
 
-app.listen(3000, () => {
-  console.log('Video service listening on port 3000');
+app.listen(port, () => {
+  console.log(`Video service listening on port http://127.0.0.1:${port}/`);
 });
