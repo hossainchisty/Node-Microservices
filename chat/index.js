264bab83aeae82368d1e50d6b0e05b8b7f0b0d8b
hossainchisty <hossain.chisty11@gmail.com>
2023-06-27 00:16:52 +0600

    refactor: Added dynamic port

--- chat/index.js
+++ chat/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const port = 3001
 
 app.get('/', (req, res) => {
   res.send(`Hello from Chat service!`);
 });
 
-app.listen(3000, () => {
-  console.log('Chat service listening on port 3000');
+app.get('/list', (req, res) => {
+  res.send(`You dont have anyone to talk😁`);
+});
+
+app.listen(port, () => {
+  console.log(`Chat service listening on port http://127.0.0.1:${port}/`);
 });
